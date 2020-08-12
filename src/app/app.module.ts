fc54bf0edec1643284934961cf63e3e7a8d434d7
camiloayza23 <camila.loayzab@gmail.com>
2020-08-12 15:11:02 -0400

    some changes on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
 import { T0807Component } from './components/t0807/t0807.component';
 import { FormsModule } from '@angular/forms';
-import { PersonaComponent } from './components/persona/persona.component';
+import { PersonaComponent } from './modules/persona/components/persona/persona.component';
 
 
 @NgModule({
