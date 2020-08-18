3da0acc4fed370e7103ecc233c34bc1395dfb8f5
camiloayza23 <camila.loayzab@gmail.com>
2020-08-18 16:14:16 -0400

    Directivas nueva conexion 08/18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ExpoPipe } from './pipes/expo.pipe';
 import { PurePipe } from './pipes/pure.pipe';
 import { ImpurePipe } from './pipes/impure.pipe';
 import { NgColorDirective } from './directives/ng-color.directive';
+import { NgFocusDirective } from './directives/ng-focus.directive';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { NgColorDirective } from './directives/ng-color.directive';
     ExpoPipe,
     PurePipe,
     ImpurePipe,
-    NgColorDirective
+    NgColorDirective,
+    NgFocusDirective
   ],
   imports: [
     //Modulos dentro de este modulo
