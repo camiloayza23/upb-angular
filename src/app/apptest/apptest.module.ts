a392876724afca8c23420385e39a0e75563fdfeb
Camila <camila.loayzab@gmail.com>
2020-08-20 16:03:24 -0400

    'new'

---
+++ src/app/apptest/apptest.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ApptestRoutingModule } from './apptest-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ApptestRoutingModule
+  ]
+})
+export class ApptestModule { }
