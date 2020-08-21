b26b3f208b817e2d6456900f49afd04bc3105214
camiloayza23 <camila.loayzab@gmail.com>
2020-08-21 16:06:29 -0400

    New module and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AboutComponent } from './components/about/about.component';
 import { Home1Component } from './components/home1/home1.component';
 import { Home2Component } from './components/home2/home2.component';
 
+
 @NgModule({
   declarations: [
     //Componentes directivas
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { UserComponent } from './components/user/user.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    UserComponent
   ]
 })
 export class UserModule { }
