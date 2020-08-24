65d2c880d7cee4cfc8c8fed479f60a0488baf81a
camiloayza23 <camila.loayzab@gmail.com>
2020-08-24 14:58:55 -0400

    Children in module navigation - 24/08

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './components/user/user.component';
+import { User1Component } from './components/user1/user1.component';
+import { User2Component } from './components/user2/user2.component';
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    User1Component,
+    User2Component
   ],
   imports: [
     CommonModule,
