55168c63dfef591f7087abdf50acc701d73c08c7
camiloayza23 <camila.loayzab@gmail.com>
2020-08-25 16:07:32 -0400

    Adding modules and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+
+  {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)},
+  {path: 'list', loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule)}
 ];
 
 @NgModule({
--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [AdminComponent]
 })
 export class AdminModule { }
