9eededa6beaf53b74ae0b976ef57f1f5795efac7
camiloayza23 <camila.loayzab@gmail.com>
2020-08-26 16:04:48 -0400

    Login working 26/08

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
-  imports: [
-    CommonModule
+  declarations: [
+    LoginComponent
   ],
-  declarations: [LoginComponent]
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ]
 })
 export class LoginModule { }
