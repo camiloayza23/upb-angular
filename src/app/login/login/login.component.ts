ac87009c28a2688bd12acbab199cca025c68d180
camiloayza23 <camila.loayzab@gmail.com>
2020-08-26 16:30:25 -0400

    Variables ng-template 26/08

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -5,7 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent 
+
+
+implements OnInit {
+  sw = false;
 
   constructor() { }
 
