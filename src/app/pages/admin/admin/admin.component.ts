e188fa5bba5aa67f5b89171e2e612f0e891e5c59
Camila <camila.loayzab@gmail.com>
2020-09-02 15:39:42 -0400

    Unsubscribe

--- src/app/pages/admin/admin/admin.component.ts
+++ src/app/pages/admin/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ProductService } from 'src/app/services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
@@ -8,7 +9,7 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+  productSubs: Subscription;
   productForm : FormGroup;
   //nameControl = new FormControl();
 
@@ -32,13 +33,17 @@ export class AdminComponent implements OnInit {
 
   onEnviar2():void{
     console.log('VALOR', this.productForm.value);
-    this.productService.addProducts(this.productForm.value).subscribe(res => {
+    this.productSubs = this.productService.addProducts(this.productForm.value).subscribe(res => {
       console.log('RESP: ', res);
       },
       err => {
         console.log('ERROR DE SERVIDOR');
       }
     );
+  }
+
+  ngOnDestroy():void {
+    this.productSubs.unsubscribe();
   }
 
 }
--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../../services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
     products = [];
-
+  productSubs :Subscription;
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.productService.getProducts().subscribe(res => {
+    this.productSubs = this.productService.getProducts().subscribe(res => {
       console.log('RES',res)
       console.log('RESPUESTA', Object.entries(res));
 
       Object.entries(res).map(p => this.products.push(p[1]));
     });
+  }
+
+  ngOnDestroy():void {
+    this.productSubs.unsubscribe();
   }
 
 }
