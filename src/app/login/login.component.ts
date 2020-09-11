1179328db61bee9b6b9fa800baf7656472573a34
Camila <camila.loayzab@gmail.com>
2020-09-11 13:16:12 -0400

    Login with user token corrections

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService  } from '../shared/services/auth.service';
+  
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../shared/services/auth.service';
 
 @Component({
-  selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent 
 
-implements OnInit {
+export class LoginComponent implements OnInit {
 
   constructor(private router: Router,
-    private authService: AuthService) {
-}
+              private authService: AuthService) {
+  }
 
   ngOnInit(): void {
   }
 
-  onLogin(form):void{
-    console.log('USERNAME:',form.value);
+  onLogin(form: any): void {
+    console.log('FORM: ', form.value);
 
     this.authService.onLogin({
       email: form.value.email,
@@ -31,7 +30,7 @@ implements OnInit {
         this.router.navigate(['pages']);
       },
       err => {
-        console.log('LOGIN ERROR: '),err;
+        console.log('LOGIN ERROR: ');
       }
     );
   }
--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -6,20 +6,22 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import {Observable} from 'rxjs';
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor{
-    constructor(){
-    }
-    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        const token = localStorage.getItem('auth');
-    
-        if (token) {
-          request = request.clone({
-            url: `${request.url}?auth=${token}`
-          });
-        }
-    
-        return next.handle(request);
-      }
 
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = localStorage.getItem('auth');
+
+    if (token) {
+      request = request.clone({
+        url: request.url.indexOf('?') > -1 ? `${request.url}&auth=${token}` : `${request.url}?auth=${token}`
+      });
+    }
+
+    return next.handle(request);
+  }
 }
